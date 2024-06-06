3e0940b4df9030c380dde428cea37f77109f47ba
YuraPerebzyak <yura.perebzyak.231998@gmail.com>
2024-06-06 22:12:47 +0300

    add hw 1

---
+++ js/talk-3.js
@@ -0,0 +1,22 @@
+function getElementWidth(content, padding, border) {
+  // Функція для видалення "px" і перетворення рядка на число
+  function parsePx(value) {
+    return parseFloat(value.replace('px', ''));
+  }
+
+  // Отримання числових значень для контенту, падінгу і бордера
+  const contentWidth = parsePx(content);
+  const paddingValue = parsePx(padding);
+  const borderValue = parsePx(border);
+
+  // Враховуючи, що box-sizing: border-box
+  // Загальна ширина елемента = ширина контенту
+  const getElementWidth = contentWidth + (paddingValue * 2) + (borderValue * 2);
+
+  return getElementWidth;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
